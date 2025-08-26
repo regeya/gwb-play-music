2857cb4ea89495182f2cc1293306d4bf0fcebc2a
regeya <regeya@gmail.com>
2025-08-25 21:22:30 -0500

    ooga

--- playing_with_midi.py
+++ playing_with_midi.py
@@ -4,18 +4,55 @@ import pygame.midi, time, random
 
 pygame.midi.init()
 
-midi_out = pygame.midi.Output(1,0)
+midi_out = pygame.midi.Output(2,0)
 midi_out.set_instrument(0)
 
 major_scale = [2,2,1,2,2,2,1]
 minor_scale = [2,1,2,2,1,2,2]
 
-starting_note = 65
-my_note = starting_note
-for i in range(8):
-    if i > 0:
-        my_note = starting_note + sum(minor_scale[0:i])
-    my_velocity = random.randint(80,127)
-    midi_out.note_on(my_note,my_velocity)
-    time.sleep(random.uniform(0.45,0.55))
-    midi_out.note_off(my_note,my_velocity)
+starting_note = 60
+#my_note = starting_note
+#for i in range(8):
+#    if i > 0:
+#        my_note = starting_note + sum(mino_scale[0:i])
+#    my_velocity = random.randint(80,127)
+#    midi_out.note_on(my_note,my_velocity)
+#    time.sleep(random.uniform(0.45,0.55))
+#    midi_out.note_off(my_note,my_velocity)
+
+my_str = "O3MBT120L8C+DEFGAB>C"
+
+sharps = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+flats = ['C', 'D-', 'D', 'E-', 'E', 'F', 'G-', 'G', 'A-', 'A', 'B-', 'B']
+all_notes = sharps + flats
+midi_notes = list(range(60, 72))
+
+notes = {k: v for k, v in zip(sharps, midi_notes)}
+notes.update({k: v for k, v in zip(flats, midi_notes)})
+
+def build_list(my_str):
+    mylist = []
+    mynum = []
+    for i in my_str:
+        if i.isdigit():
+            mynum.append(i)
+        elif i == "#" or i == "+":
+            mylist[-1] = mylist[-1] + "#"
+        elif i == "-":
+            mylist[-1] = mylist[-1] + "-"
+        elif mylist and mylist[-1] == "M":
+            mylist[-1] = mylist[-1] + i
+        else:
+            if mynum:
+                mylist.append("".join(mynum))
+                mynum = []
+            mylist.append(i)
+    return mylist
+
+notes_to_parse = build_list(my_str)
+
+for i in ['C','D','E', 'F','G','A','B']:
+    mynote = notes[i]
+    midi_out.note_on(mynote, 96)
+    time.sleep(1)
+    midi_out.note_off(mynote, 96)
--- python_play_music.py
+++ python_play_music.py
@@ -8,24 +8,21 @@ from pygame.locals import *
 import time
 import re
 
-import math
 import numpy
 from scipy import signal as sg
-import struct
-import string
 
+CHUNK = 1024
 
-
-CHUNK=1024
 
 class PlayStatement:
     """an attempt to write a GW-BASIC style PLAY, complete with square-wave audio"""
+
     bits = 16
     tempo = 120
     a3 = 440
-    octave = 3 # default to 3
+    octave = 3  # default to 3
     sample_rate = 44100
-    max_sample = 2**(bits - 1) - 1
+    max_sample = 2 ** (bits - 1) - 1
     default_volume = 15
     vol_steps = int(max_sample / 15)
     current_volume = default_volume * vol_steps
@@ -37,7 +34,7 @@ class PlayStatement:
     major_notes = "cdefgab"
     starting_note = -9
     intervals = [2, 2, 1, 2, 2, 2, 1]
-    notes_range={}
+    notes_range = {}
     note = starting_note
     statement = ""
 
@@ -51,17 +48,23 @@ class PlayStatement:
         self.array = []
 
     def play_note(self, i, sharp):
-        frequency = self.a3 * (2 ** ((self.notes_range[i]+self.modifier+sharp)/12.0))
+        frequency = self.a3 * (
+            2 ** ((self.notes_range[i] + self.modifier + sharp) / 12.0)
+        )
         print(frequency)
-        self.duration = int((self.sample_rate * 60.0) / (self.tempo * (self.notelen / 4.0)))
-        return self.current_volume*sg.square(2 * numpy.pi * frequency * numpy.arange(self.duration) / self.sample_rate).astype(numpy.int16)
+        self.duration = int(
+            (self.sample_rate * 60.0) / (self.tempo * (self.notelen / 4.0))
+        )
+        return self.current_volume * sg.square(
+            2 * numpy.pi * frequency * numpy.arange(self.duration) / self.sample_rate
+        ).astype(numpy.int16)
 
     def parse_string(self, play_string):
         tokenized = [f for f in re.findall("([a-z]|[0-9]+|[#+-><])", play_string) if f]
         myarray = []
         for j, i in enumerate(tokenized):
             try:
-                k = tokenized[j+1]
+                k = tokenized[j + 1]
             except:
                 k = None
             sharp = 0
@@ -90,7 +93,7 @@ class PlayStatement:
             elif i == "p":
                 if k:
                     self.notelen = int(k)
-                    f = 60.0/(self.tempo*(self.notelen/4.0))
+                    f = 60.0 / (self.tempo * (self.notelen / 4.0))
                     print(f)
                     time.sleep(f)
             elif i in self.major_notes:
@@ -102,19 +105,22 @@ class PlayStatement:
                     elif k.isdigit():
                         self.notelen = int(k)
                 x = self.play_note(i, sharp)
-                print (i, k)
+                print(i, k)
                 sound = pygame.sndarray.make_sound(x)
                 sound.play()
-                time.sleep(len(x)/(self.sample_rate * 1.0))
+                time.sleep(len(x) / (self.sample_rate * 1.0))
                 self.notelen = 4
 
     def sound(self):
-         self.parse_string(self.statement)
+        self.parse_string(self.statement)
 
-m = ["E8 E8 F8 G8 G8 F8 E8 D8 C8 C8 E8 E8 E8 D12 D4",
-     "E8 E8 F8 C8 G8 F8 E8 D8 C8 C8 D8 E8 D8 C12 C4",
-     "D8 D8 E8 C8 D8 E12 F12 E8 C8 D8 E12 F12 E8 D8 C8 D8 P8",
-     "E8 E8 F8 G8 G8 F8 E8 D8 C8 C8 D8 E8 D8 C12 C4"]
+
+m = [
+    "E8 E8 F8 G8 G8 F8 E8 D8 C8 C8 E8 E8 E8 D12 D4",
+    "E8 E8 F8 C8 G8 F8 E8 D8 C8 C8 D8 E8 D8 C12 C4",
+    "D8 D8 E8 C8 D8 E12 F12 E8 C8 D8 E12 F12 E8 D8 C8 D8 P8",
+    "E8 E8 F8 G8 G8 F8 E8 D8 C8 C8 D8 E8 D8 C12 C4",
+]
 
 for i in m:
     play = PlayStatement()
