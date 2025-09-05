b18ea16156ace8d1d464b0fae76806fb4798490b
regeya <regeya@gmail.com>
2025-09-04 22:44:57 -0500

    adding controller code
    going to migrate code from squarewave generator to this one

--- playing_with_midi.py
+++ playing_with_midi.py
@@ -30,24 +30,22 @@ try:
     midi_out = pygame.midi.Output(device_id)
 except pygame.midi.MidiException as e:
     print(f"An error occurred: {e}")
-finally:
-    print("Done.")
 
 # midi_out = pygame.midi.Output(0, 0)
 # midi_out.set_instrument(0)
 
-major_scale = [2, 2, 1, 2, 2, 2, 1]
-minor_scale = [2, 1, 2, 2, 1, 2, 2]
-
-starting_note = 60
-my_note = starting_note
-for i in range(8):
-    if i > 0:
-        my_note = starting_note + sum(minor_scale[0:i])
-    my_velocity = random.randint(80, 127)
-    midi_out.note_on(my_note, my_velocity)
-    time.sleep(random.uniform(0.45, 0.55))
-    midi_out.note_off(my_note, my_velocity)
+# major_scale = [2, 2, 1, 2, 2, 2, 1]
+# minor_scale = [2, 1, 2, 2, 1, 2, 2]
+#
+# starting_note = 60
+# my_note = starting_note
+# for i in range(8):
+#    if i > 0:
+#        my_note = starting_note + sum(minor_scale[0:i])
+#    my_velocity = random.randint(80, 127)
+#    midi_out.note_on(my_note, my_velocity)
+#    time.sleep(random.uniform(0.45, 0.55))
+#    midi_out.note_off(my_note, my_velocity)
 
 my_str = "O3MBT120L8C+DEFGAB>C"
 
@@ -58,6 +56,8 @@ midi_notes = list(range(60, 72))
 
 notes = {k: v for k, v in zip(sharps, midi_notes)}
 notes.update({k: v for k, v in zip(flats, midi_notes)})
+
+print(notes)
 
 
 def build_list(my_str):
@@ -82,11 +82,11 @@ def build_list(my_str):
 
 notes_to_parse = build_list(my_str)
 
-# for i in ["C", "D", "E", "F", "G", "A", "B"]:
-#    mynote = notes[i]
-#    midi_out.note_on(mynote, 96)
-#    time.sleep(1)
-#    midi_out.note_off(mynote, 96)
-#
+for i in ["C", "D", "E", "F", "G", "A", "B"]:
+    mynote = notes[i]
+    midi_out.note_on(mynote, 96)
+    time.sleep(1)
+    midi_out.note_off(mynote, 96)
+
 midi_out.close()
 pygame.midi.quit()
